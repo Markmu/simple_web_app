8b17ba8061cc078d555b8363cfb6fb557570eb13
Mark Mu <muchao1303@gmail.com>
2016-01-20 20:19:03 +0800

    add some functional test and annotations

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,7 +1,26 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
 
-browser.get('http://localhost:8000')
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #性感的艾丽听说有一个很酷的待办事项应用
+        #早上，她睁开惺忪的睡眼半裸着酥胸去看了这个应用的首页
+        self.browser.get('http://localhost:8000')
+
+        #她抬起修长的手臂伸了个懒腰，注意到网页的标题和头部都包含“To-Do”这个词
+        self.assertIn('To-Do',self.browser.title)
+        self.fail('Finish the test!')
+
+        #应用邀请性感美丽的她输入一个待办事项
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
