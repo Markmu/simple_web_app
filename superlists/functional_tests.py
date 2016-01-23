9edd4ad976ac8bab253542f82965af946cdb6fc3
Markmu <muchao1303@gmail.com>
2016-01-24 02:14:58 +0800

    Refactor home page view to use a template and a little bit generated front page HTML

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,10 +18,38 @@ class NewVisitorTest(unittest.TestCase):
 
         #她抬起修长的手臂伸了个懒腰，注意到网页的标题和头部都包含“To-Do”这个词
         self.assertIn('To-Do',self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         #应用邀请性感美丽的她输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
 
+        # 她在文本框中输入了购买一套情趣内衣
+        # 拥有魔鬼身材的性感的爱丽的一个爱好是穿着骚骚的情趣内衣在镜子前自恋
+        inputbox.send_keys('购买一套超级性感的情趣内衣')
+
+        # 她按下回车键之后，页面更新了
+        # 代办事项表格中显示了 “1：购买一套超级性感的情趣内衣”
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: 购买一套超级性感的情趣内衣' for row in rows),
+                'New to-do item did not appear in table'
+        )
+
+        # 页面中又显示了一个文本框，可以输入其它待办事项
+        # 她输入了穿情趣内衣发自拍吸引网友
+        # 没错，艾丽就是这么骚！！！
+        self.fail('Finish the test!')
+
+        # 页面再次更新，她的清单中显示了这两个待办事项
+        # ......
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
