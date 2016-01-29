6ad7395febc8c88a6ddfd9de052948755ed16b08
Markmu <muchao1303@gmail.com>
2016-01-29 21:38:17 +0800

    First steps of FT for layout + styling

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -84,3 +84,15 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # 两个人都很满意，去睡觉了
+
+    def test_layout_and_styling(self):
+        # 访问首页
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        # 看到输入框居中显示
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
