00c43f1049a03a7038258edf9ff5a70e5dcf3aea
Markmu <muchao1303@gmail.com>
2016-02-04 20:50:27 +0800

    move sqlite database outside of main source tree

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -2,7 +2,23 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+import sys
+
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +35,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 性感的艾丽听说有一个很酷的待办事项应用
         # 早上，她睁开惺忪的睡眼半裸着酥胸去看了这个应用的首页
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         #她抬起修长的手臂伸了个懒腰，注意到网页的标题和头部都包含“To-Do”这个词
         self.assertIn('To-Do',self.browser.title)
@@ -63,7 +79,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # 弗朗西斯访问首页
         # 页面中看不到艾丽的清单
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('购买', page_text)
         self.assertNotIn('穿上', page_text)
@@ -87,7 +103,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # 访问首页
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
         # 看到输入框居中显示
         inputbox = self.browser.find_element_by_id('id_new_item')
