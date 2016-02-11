31fe155b262111b4c7e419cf1a3a6f31f4f72d35
Markmu <muchao1303@gmail.com>
2016-02-11 21:40:36 +0800

    Split out unit tests into two files

---
+++ source/lists/tests/test_model.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from lists.models import Item, List
+# Create your tests here.
+
+
+class ListAndItemModelsTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.list = list_
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.list = list_
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        saved_item = Item.objects.all()
+        self.assertEqual(saved_item.count(),2)
+
+        first_saved_item = saved_item[0]
+        second_saved_item = saved_item[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_item.list, list_)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_item.list, list_)
--- source/lists/tests/test_view.py
+++ source/lists/tests/test_view.py
@@ -72,34 +72,6 @@ class NewItemTest(TestCase):
         response = self.client.get('/lists/%d/' % (correct_list.id,))
         self.assertEqual(response.context['list'], correct_list)
 
-class ListAndItemModelsTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_item = Item.objects.all()
-        self.assertEqual(saved_item.count(),2)
-
-        first_saved_item = saved_item[0]
-        second_saved_item = saved_item[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_item.list, list_)
 
 class ListViewTest(TestCase):
 
