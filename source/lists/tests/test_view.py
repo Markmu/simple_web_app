c6c4dad69d2b902de5ac1dd8c8b5ac31549b5a1a
Markmu <muchao1303@gmail.com>
2016-02-11 23:49:33 +0800

    enforce model validation in list item

--- source/lists/tests/test_view.py
+++ source/lists/tests/test_view.py
@@ -110,7 +110,7 @@ class ListViewTest(TestCase):
     def test_validation_errors_end_up_on_lists_page(self):
         list_ = List.objects.create()
         response = self.client.post(
-            '/lists/%d/' % (list_.id,)
+            '/lists/%d/' % (list_.id,),
             data={'item_text': ''}
         )
         self.assertEqual(response.status_code, 200)
