418a2ae57508f31ef81ca08a62f1e6ba56dd9116
Markmu <muchao1303@gmail.com>
2016-02-12 23:47:06 +0800

    Implement duplicate item validation at model layer

---
+++ source/lists/migrations/0005_list_item_unique_together.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lists', '0004_item_list'),
+    ]
+
+    operations = [
+        migrations.AlterUniqueTogether(
+            name='item',
+            unique_together=set([('list', 'text')]),
+        ),
+    ]
--- source/lists/models.py
+++ source/lists/models.py
@@ -10,3 +10,6 @@ class List(models.Model):
 class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
+
+    class Meta:
+        unique_together = ('list','text')
