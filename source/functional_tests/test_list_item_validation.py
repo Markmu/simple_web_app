77f0e07b6b4d60b5f8ae433f81f5bcb6b719a617
Markmu <muchao1303@gmail.com>
2016-02-13 21:51:51 +0800

    cleared error massages on input when it happened

--- source/functional_tests/test_list_item_validation.py
+++ source/functional_tests/test_list_item_validation.py
@@ -3,6 +3,9 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         # 艾丽范文首页，不小心提交了一个空待办事项
         # 输入框中没输入内容，她就按下了回车键
@@ -11,7 +14,7 @@ class ItemValidationTest(FunctionalTest):
 
         # 首页刷新了，显示了一个错误消息
         # 提示待办事项不能为空
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, 'You can\'t have an empty list item.')
 
         # 她输入一些文字，然后再次提交，这次没问题了
@@ -23,7 +26,7 @@ class ItemValidationTest(FunctionalTest):
 
         # 在清单页面她看到了一个类似的错误消息
         self.check_for_row_in_list_table('1: some words')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, 'You can\'t have an empty list item.')
         # 输入文字之后就没有问题了
         self.get_item_input_box().send_keys('some other words\n')
@@ -41,5 +44,19 @@ class ItemValidationTest(FunctionalTest):
 
         # 她看到一条有帮助的错误消息
         self.check_for_row_in_list_table('1: shopping')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You've already got this in your list.")
+
+    def test_error_messages_are_cleared_on_input(self):
+        # 艾丽新建一个清单，但方法不对出现了一个错误
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        # 为了消除错误，她开始在输入框中输入内容
+        self.get_item_input_box().send_keys('a')
+
+        # 看到错误消息消失了，她很高兴
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
